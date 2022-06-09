54e04f8ff41f514a8b6c235a8baceb2c54e7aa42
rohankveer <rohan.veer1@hp.com>
2022-06-09 14:29:23 +0530

    Load data from config file

--- app/components/MainLayout.jsx
+++ app/components/MainLayout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import { SideBar } from './SideBar';
+import Config from '../config/config.json';
 
 function MainLayout() {
+  console.log(Config);
   return (
     <div id='main-wrapper'>
       <SideBar />
+      <h4>{Config.appName}</h4>
       <Outlet />
     </div>
   );

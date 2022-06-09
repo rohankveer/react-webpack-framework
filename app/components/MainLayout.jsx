127f6f0228612debcb035aec94363e9ae7403190
rohankveer <rohan.veer1@hp.com>
2022-06-09 15:28:19 +0530

    Localization support

--- app/components/MainLayout.jsx
+++ app/components/MainLayout.jsx
@@ -3,11 +3,12 @@ import { SideBar } from './SideBar';
 import Config from '../config/config.json';
 
 function MainLayout() {
-  console.log(Config);
   return (
     <div id='main-wrapper'>
       <SideBar />
       <h4>{Config.appName}</h4>
+      <h4>{L10n.t('hello')}</h4>
+      <h4>{L10n.t('dynamicVariables', { variable: 'Hello' })}</h4>
       <Outlet />
     </div>
   );

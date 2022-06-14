916dfe84af4fe2f36ec2868d117eea628d6fe8c4
rohankveer <rohan.veer1@hp.com>
2022-06-14 15:16:22 +0530

    dist folder fix

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,15 +1,16 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair
 /* eslint-disable no-undef */
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpack = require('webpack');
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var webpack = require('webpack')
 
 module.exports = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '..', 'dist'),
     chunkFilename: '[name].[contenthash].bundle.js',
   },
   optimization: {
@@ -23,16 +24,16 @@ module.exports = {
         minify: {
           removeAttributeQuotes: true,
           collapseWhitespace: true,
-          removeComments: true
-        }
-      })
-    ]
+          removeComments: true,
+        },
+      }),
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new webpack.DefinePlugin({
-      __DEBUG__: false
-    })
+      __DEBUG__: false,
+    }),
   ],
-  target: 'browserslist'
+  target: 'browserslist',
 }
